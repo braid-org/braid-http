6c3cf67e1fc4f653e20a95a45e6ba60b48c06696
dglittle <dglittle@users.noreply.github.com>
2025-07-10 17:43:38 -0600

    1.3.78 - adds support for alternate encoding blocks within a 209

--- braid-http-client.js
+++ braid-http-client.js
@@ -612,7 +612,7 @@ var subscription_parser = (cb) => ({
 function parse_update (state) {
     // If we don't have headers yet, let's try to parse some
     if (!state.headers) {
-        var parsed = parse_headers(state.input)
+        var parsed = parse_headers(state.input, true)
 
         // If header-parsing fails, send the error upstream
         if (parsed.result === 'error')
@@ -636,12 +636,52 @@ function parse_update (state) {
 }
 
 // Parsing helpers
-function parse_headers (input) {
+function parse_headers (input, check_for_encoding_blocks) {
 
     // Find the start of the headers
     var start = 0
     while (input[start] === 13 || input[start] === 10) start++
     if (start === input.length) return {result: 'waiting'}
+
+    // Check for an "Encoding" block like this:
+    //
+    //   Encoding: dt
+    //   Length: 411813
+    //   <binary dt file>
+    //
+    // Such a block will start with an "e", not an "h"
+    if (check_for_encoding_blocks &&
+        (input[start] === 101 || input[start] === 69)) {
+
+        // Look for two newlines
+        var end = start
+        var count = 0
+        while (++end) {
+            if (end > input.length) return {result: 'waiting'}
+            if (input[end - 1] === 10) count++
+            if (count === 2) break
+        }
+
+        // Extract the header string
+        var headers_source = input.slice(start, end).map(x => String.fromCharCode(x)).join('')
+
+        // Parse
+        var m = headers_source.match(/Encoding:\s*(\w+)\r?\nLength:\s*(\d+)\r?\n/i)
+        if (!m) return {
+            result: 'error',
+            message: `Parse error in encoding block: ${JSON.stringify(headers_source)}`
+        }
+        var headers = {
+            encoding: m[1],
+            length: m[2]
+        }
+
+        // Update the input
+        input = input.slice(end)
+
+        // And return the parsed result
+        return { result: 'success', headers, input }
+    }
 
     // Look for the double-newline at the end of the headers.
     var end = start
@@ -714,7 +754,8 @@ function parse_body (state) {
 
     // Parse Body Snapshot
 
-    var content_length = parseInt(state.headers['content-length'])
+    var content_length = parseInt(state.headers['content-length'] ??
+        (state.headers.patches === undefined && state.headers['length']))
     if (!isNaN(content_length)) {
 
         // We've read a Content-Length, so we have a block to parse
