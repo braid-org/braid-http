aac888d762a46c83d47405ddc41d2edf143483a0
Michael Toomim <toomim@gmail.com>
2026-03-25 00:31:18 +0900

    1.3.110 - Add typescript type exports

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+import { IncomingMessage, ServerResponse } from 'http';
+
+type RequestHandler = (req: IncomingMessage, res: ServerResponse) => void;
+
+export declare function http_server(handler: RequestHandler): RequestHandler;
+export declare function http_server(req: IncomingMessage, res: ServerResponse, next?: () => void): void;
+
+export declare function free_cors(res: ServerResponse): void;
+
+export declare function fetch(url: string, params?: Record<string, any>): Promise<Response>;
+
+export declare function http_client(httpModule: any): any;
